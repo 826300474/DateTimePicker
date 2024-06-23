69af68a6a93e2e8e33de32d9266c972708da724c
柴志远 <chaizhiyuan@vsan.cn>
2024-06-23 19:49:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,247 @@
+<script setup>
+	import TimePicker from '../src/components/TimePicker.vue';
+	import {
+		ref,
+		computed,
+		reactive,
+		watch
+	} from 'vue';
+
+	import moment from 'moment';
+
+	const icon_Map = [{
+		icon: '<<',
+		actionKey: 'preYear',
+	}, {
+		icon: '<',
+		actionKey: 'preMonth',
+	}, {
+		icon: '>',
+		actionKey: 'nextMonth',
+	}, {
+		icon: '>>',
+		actionKey: 'nextYear',
+	}]
+
+	const curtime = ref(moment().startOf('day'));
+
+	const state = reactive({
+		value: [],
+	})
+
+	const onIconClick = (e) => {
+		switch (e.target.getAttribute('data-key')) {
+			case 'preYear':
+				curtime.value = moment(curtime.value).add(-1, 'year');
+				break;
+			case 'preMonth':
+				curtime.value = moment(curtime.value).add(-1, 'month');
+				break;
+			case 'nextMonth':
+				curtime.value = moment(curtime.value).add(1, 'month');
+				break;
+			case 'nextYear':
+				curtime.value = moment(curtime.value).add(1, 'year');
+				break;
+			default:
+				break;
+		}
+	}
+
+	const onDayClick = (e) => {
+		const curTime = daySize.value[e.target.getAttribute('data-index')].date;
+		if (e.target.classList.contains('active')) {
+			const index = state.value.findIndex(el => el.isSame(curTime));
+			state.value.splice(index, 1);
+			return;
+		}
+		if (state.value.length === 2) {
+			return;
+		}
+		if (state.value.length === 1) {
+			if (curTime.isBefore(state.value[0])) {
+				state.value.unshift(curTime);
+			} else {
+				state.value.push(curTime);
+			}
+			return;
+		}
+		state.value.push(curTime);
+	}
+
+	const daySize = computed(() => {
+		let firstDayOfMonth = moment(curtime.value).clone().startOf('month');
+		let lastDayOfMonth = moment(curtime.value).clone().endOf('month');
+		let daysInMonth = lastDayOfMonth.diff(firstDayOfMonth, 'days') + 1;
+		const res = [...(new Array(firstDayOfMonth.day()).fill({})), {
+			value: 1,
+			date: firstDayOfMonth
+		}];
+		for (var i = 1; i < daysInMonth; i++) {
+			res.push({
+				value: i + 1,
+				date: moment(curtime.value).clone().startOf('month').add(i, 'day')
+			});
+		}
+		return res;
+	});
+</script>
+
+<template>
+	<div class="box">
+		<div class="top" @click="onIconClick">
+			<span class="icon" :data-key="icon_Map[0].actionKey">{{ icon_Map[0].icon }}</span>
+			<span class="icon" :data-key="icon_Map[1].actionKey">{{ icon_Map[1].icon }}</span>
+			<span>{{ curtime.format('YYYY年 MM月') }}</span>
+			<span class="icon" :data-key="icon_Map[2].actionKey">{{ icon_Map[2].icon }}</span>
+			<span class="icon" :data-key="icon_Map[3].actionKey">{{ icon_Map[3].icon }}</span>
+		</div>
+		<div class="mid">
+			<div class="item title">
+				<span>S</span>
+				<span>M</span>
+				<span>T</span>
+				<span>W</span>
+				<span>T</span>
+				<span>F</span>
+				<span>S</span>
+			</div>
+			<div class="item" @click="onDayClick">
+				<span class="day" :data-index="i" :class="{ 
+						current : el.date && moment().startOf('day').isSame(el.date),
+						include : el.date && state.value.length === 2 && el.date.isBetween(state.value[0], state.value[1], null, '[]'),
+						includeFirst : el.date && state.value.length === 2 && state.value[0].isSame(el.date),
+						includeLast : el.date && state.value.length === 2 && state.value[1].isSame(el.date),
+						active:el.date && state.value.findIndex(v => v.isSame(el.date)) > -1
+					}" v-for="(el,i) in daySize" :key="el.date">{{ el.value ? el.value:null }}</span>
+			</div>
+		</div>
+		<div class="bottom">
+			<div class="li">
+				开始时间
+				<TimePicker />
+			</div>
+			<div class="li">
+				结束时间
+				<TimePicker />
+			</div>
+		</div>
+	</div>
+</template>
+
+<style>
+	.box {
+		width: 280px;
+		padding: 20px;
+	}
+
+	.top {
+		font-weight: bold;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.mid {
+		margin-top: 20px;
+	}
+
+	.icon {
+		cursor: pointer;
+	}
+
+	.item {
+		display: flex;
+		flex-wrap: wrap;
+		margin-top: 12px;
+		width: 280px;
+	}
+
+	.item span {
+		text-align: center;
+		flex: 0 0 auto;
+		width: 38px;
+		height: 38px;
+		border: solid 1px rgba(0, 0, 0, 0);
+		line-height: 40px;
+		box-sizing: border-box;
+		margin-bottom: 10px;
+		border-radius: 50%;
+		position: relative;
+	}
+
+	.item span:hover {
+		color: purple;
+	}
+
+	.title {
+		color: #808080;
+	}
+
+
+	.item .day {
+		cursor: pointer;
+	}
+
+	.item .current {
+		border: solid 1px purple;
+	}
+
+	.item .active {
+		background-color: purple;
+		color: #fff;
+	}
+
+	.item .active:hover {
+		color: #fff;
+	}
+
+
+
+	.item .include::after {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 38px;
+		height: 38px;
+		background-color: purple;
+		opacity: 0.1;
+		z-index: -1;
+	}
+
+	.item .includeFirst:after {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 19px;
+		width: 19px;
+		height: 38px;
+		background-color: purple;
+		opacity: 0.1;
+		z-index: -1;
+	}
+
+	.item .includeLast:after {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 19px;
+		height: 38px;
+		background-color: purple;
+		opacity: 0.1;
+		z-index: -1;
+	}
+
+	.bottom {
+		margin-top: 12px;
+		padding-top: 12px;
+		border-top: solid 1px #f2f2f2;
+	}
+
+	.bottom .li {
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: 20px;
+	}
+</style>
---
+++ src/components/TimePicker.vue
@@ -0,0 +1,181 @@
+<script setup>
+	import {
+		ref,
+		watch,
+		onMounted,
+		onUnmounted
+	} from 'vue';
+
+	import {
+		debounce
+	} from 'lodash';
+	import moment from 'moment';
+
+	const fillZero = (el) => `0${el}`.slice(-2);
+
+	const isHouerRef = (e) => (e.currentTarget || e.target).classList.contains('selectionBoxHouer')
+
+	const houerList = Array.from({
+		length: 24
+	}).map((_, index) => index + 1);
+
+	const minuteList = Array.from({
+		length: 60
+	}).map((_, index) => index + 1);
+
+	const houer = ref(0);
+	const minute = ref(0);
+	const houerRef = ref();
+	const minuteRef = ref();
+	
+	const handleScroll = function(event){
+		const cur = Math.ceil((event.target.scrollTop + 90) / 30) - 2;
+		if (isHouerRef(event)) {
+			houer.value = cur;
+		} else {
+			minute.value = cur;
+		}
+		scrollTo(cur, event.target);
+	}
+
+	onMounted(() => {
+		let now = moment();
+		houer.value = now.hour();
+		minute.value = now.minute();
+		houerRef.value.addEventListener('scroll', debounce(handleScroll, 200));
+		minuteRef.value.addEventListener('scroll', debounce(handleScroll, 200));
+	});
+
+	onUnmounted(() => {
+		houerRef.value.removeEventListener('scroll', handleScroll);
+		minuteRef.value.removeEventListener('scroll', handleScroll);
+	})
+
+	watch(houer, () => {
+		scrollTo(houer.value, houerRef.value);
+	});
+
+	watch(minute, () => {
+		scrollTo(minute.value, minuteRef.value);
+	});
+
+	const scrollTo = (val, houerRef) => {
+		houerRef.scrollTo(0, (val - 1) * 30)
+	}
+
+	const handleMouseEnter = () => {
+		scrollTo(houer.value, houerRef.value);
+		scrollTo(minute.value, minuteRef.value);
+	}
+
+	const onClick = (e) => {
+		if (e.target.getAttribute('data-index')) {
+			if (isHouerRef(e)) {
+				houer.value = Number(e.target.getAttribute('data-index'));
+			} else {
+				minute.value = Number(e.target.getAttribute('data-index'));
+			}
+		}
+	}
+</script>
+
+<template>
+	<div class="time" @mouseenter="handleMouseEnter">
+		<span class="timeItem">{{ fillZero(houer) }}</span> : <span class="timeItem">{{ fillZero(minute) }}</span>
+		<div class="selectionBox">
+			<div class="line line1"></div>
+			<div class="line line2"></div>
+			<div class="selectionBoxItem selectionBoxHouer" @click="onClick" ref="houerRef">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div :data-index="el" :class="{
+					active:houer === el
+				}" v-for="el in houerList">{{ fillZero(el) }}</div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			<div class="selectionBoxItem selectionBoxMinute" @click="onClick" ref="minuteRef">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div :data-index="el" :class="{
+					active:minute === el
+				}" v-for="el in minuteList">{{ fillZero(el) }}</div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+	.time {
+		cursor: pointer;
+		position: relative;
+	}
+
+	.time:hover .selectionBox {
+		display: flex;
+	}
+
+
+	.selectionBox {
+		position: absolute;
+		right: 0;
+		bottom: 100%;
+		display: none;
+		flex-direction: row;
+		background-color: #fff;
+		border: solid 1px #f2f2f2;
+	}
+
+	.line {
+		width: 100%;
+		height: 1px;
+		background-color: #f2f2f2;
+		position: absolute;
+		top: 90px;
+		left: 0;
+	}
+
+	.line2 {
+		width: 100%;
+		height: 1px;
+		background-color: #f2f2f2;
+		position: absolute;
+		top: 120px;
+		left: 0;
+	}
+
+
+	.selectionBox .selectionBoxItem {
+		width: 50px;
+		height: 210px;
+		overflow-y: auto;
+		padding: 0 12px;
+	}
+
+	.selectionBox .selectionBoxItem div {
+		height: 30px;
+		line-height: 30px;
+		text-align: center;
+		cursor: pointer;
+	}
+
+	.selectionBox .selectionBoxItem .active {
+		font-weight: bold;
+	}
+
+	/* 	.selectionBox .selectionBoxItem div:hover {
+		background-color: #f2f2f2;
+	} */
+
+	.time .timeItem {
+		background: #f2f2f2;
+		padding: 4px;
+		border-radius: 4px;
+	}
+</style>
